eece91d0841bc515056c717e2e25a4382de05910
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2019-11-27 17:39:06 -0300

    refactor: 💡 change fauna request to its own file

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -1,40 +1,9 @@
 import { NowRequest, NowResponse } from '@now/node';
-import axios from 'axios';
-
-const secret = process.env.FAUNADB_SECRET_KEY;
-const b64encodedSecret = Buffer.from(secret + ':').toString('base64');
-
-const fauna = 'https://graphql.fauna.com/graphql';
-axios.defaults.baseURL = fauna;
-axios.defaults.headers.common['Authorization'] = `Basic ${b64encodedSecret}`;
-
-const query = `{
-    allOrders {
-        data {
-            _id
-            status
-            shipAddress {
-                zipCode
-            }
-            line {
-                product {
-                    name
-                    description
-                }
-                quantity
-                price
-            }
-            customer {
-                firstName
-                lastName
-            }
-        }
-    }
-}`;
+import { loadOrders } from '../../lib/fauna-client';
 
 export default async (_: NowRequest, response: NowResponse) => {
   try {
-    const data = await axios.post('', { query });
+    const data = await loadOrders();
     response.status(200).json(data.data);
   } catch (e) {
     // something went wrong
