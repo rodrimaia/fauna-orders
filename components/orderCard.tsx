bae561c179031178ee363073bcc1f37aaea8e4d4
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2019-11-28 13:26:22 -0300

    refactor: 💡 extract and move ProductsTable to its own file

--- components/orderCard.tsx
+++ components/orderCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FunctionComponent } from 'react';
-import { Order, Line } from '../types';
+import { Order } from '../types';
+import ProductsTable from '../components/productsTable';
 
 interface CardOrderProps {
   order: Order;
@@ -15,7 +16,9 @@ const OrderCard: FunctionComponent<CardOrderProps> = ({ order }) => {
       }}
     >
       <div className="box-line">
-        <span className="box-line-name is-size-7-mobile">{order.customer.name}</span>
+        <span className="box-line-name is-size-7-mobile">
+          {order.customer.name}
+        </span>
         <span className="box-line-products has-text-grey has-text-weight-light is-size-7-mobile">
           {order.line.length} Products
         </span>
@@ -29,42 +32,16 @@ const OrderCard: FunctionComponent<CardOrderProps> = ({ order }) => {
       </div>
       <div className={`card-content ${isHidden ? 'is-hidden' : ''}`}>
         <div className="content">
-          <div className="container">
-            <p>Products </p>
-            <div className="table-container">
-            <table className="table is-narrow is-hoverable is-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Description</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                  <th>Subtotal</th>
-                </tr>
-              </thead>
-              <tbody>
-                {order.line.map((l: Line) => (
-                  <tr className="has-text-weight-light" key={l.product.name}>
-                    <td>{l.product.name}</td>
-                    <td>{l.product.description}</td>
-                    <td>{l.quantity}</td>
-                    <td>{l.price.toFixed(2)}</td>
-                    <td>{l.total.toFixed(2)}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            </div>
-            <hr className="hr" />
-            <p>Ship To</p>
-            <p className="has-text-weight-light" style={{ marginBottom: 0 }}>
-              {order.shipAddress.street}
-            </p>
-            <p className="has-text-weight-light">
-              {order.shipAddress.city} {order.shipAddress.state}{' '}
-              {order.shipAddress.zipCode}
-            </p>
-          </div>
+          <ProductsTable lines={order.line} />
+          <hr className="hr" />
+          <p>Ship To</p>
+          <p className="has-text-weight-light" style={{ marginBottom: 0 }}>
+            {order.shipAddress.street}
+          </p>
+          <p className="has-text-weight-light">
+            {order.shipAddress.city} {order.shipAddress.state}{' '}
+            {order.shipAddress.zipCode}
+          </p>
         </div>
       </div>
 
---
+++ components/productsTable.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from 'react';
+import { Line } from '../types';
+
+interface ProductsTableProps {
+  lines: Line[];
+}
+
+const ProductsTable: FunctionComponent<ProductsTableProps> = ({ lines }) => {
+  return (
+    <div className="container">
+      <p>Products </p>
+      <div className="table-container">
+        <table className="table is-narrow is-hoverable is-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lines.map((l: Line) => (
+              <tr className="has-text-weight-light" key={l.product.name}>
+                <td>{l.product.name}</td>
+                <td>{l.product.description}</td>
+                <td>{l.quantity}</td>
+                <td>{l.price.toFixed(2)}</td>
+                <td>{l.total.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsTable;
