7666f1234540c8598d1243231e18fe6bd1b6b4fa
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2019-11-28 10:57:57 -0300

    refactor: 💡 Move orderCards to its own file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios, { AxiosResponse } from 'axios';
 import useSWR from 'swr';
 import Nav from '../components/nav';
+import OrderCard from '../components/orderCard';
 import Head from 'next/head';
-import { Order, Line } from '../types';
+import { Order } from '../types';
 
 interface EndpointResponse {
   data: Order[];
@@ -44,7 +45,7 @@ const Home = () => {
 
           <div className="container">
             {data.data.data.map(order => (
-              <Card key={order._id} order={order} />
+              <OrderCard key={order._id} order={order} />
             ))}
           </div>
         </div>
@@ -63,84 +64,6 @@ const Home = () => {
         }
         .title {
           color: #6474af;
-        }
-      `}</style>
-    </div>
-  );
-};
-
-const Card = ({ order }) => {
-  const [isHidden, setIsHidden] = useState(true);
-  return (
-    <div
-      className="box is-fullwidth"
-      onClick={() => {
-        setIsHidden(!isHidden);
-      }}
-    >
-      <div className="box-line">
-        <span className="box-line-name">{order.customer.name}</span>
-        <span className="box-line-products has-text-grey has-text-weight-light is-size-7">
-          {order.line.length} Products
-        </span>
-        <span className="box-line-status has-text-grey has-text-weight-light is-size-7">
-          {order.status}
-        </span>
-        <span className="box-line-price has-text-primary is-size-4">
-          {order.totalPrice.toFixed(2)}
-        </span>
-        <i className={`fa fa-angle-${isHidden ? 'down' : 'up'}`}></i>
-      </div>
-      <div className={`card-content ${isHidden ? 'is-hidden' : ''}`}>
-        <div className="content">
-          <div className="container">
-            <p>Products </p>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Description</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                  <th>Subtotal</th>
-                </tr>
-              </thead>
-              <tbody>
-                {order.line.map((l: Line) => (
-                  <tr key={l.product.name}>
-                    <td>{l.product.name}</td>
-                    <td>{l.product.description}</td>
-                    <td>{l.quantity}</td>
-                    <td>{l.price.toFixed(2)}</td>
-                    <td>{l.total.toFixed(2)}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <hr className="hr" />
-            <p>Ship To</p>
-            <p>{order.shipAddress.street}</p>
-            <p>
-              {order.shipAddress.city} {order.shipAddress.state}{' '}
-              {order.shipAddress.zipCode}
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <style jsx>{`
-        .box {
-          cursor: pointer;
-        }
-        .box-line {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-        }
-        .box-line-name {
-          width: 200px;
-        }
-        .box-line products {
         }
       `}</style>
     </div>
